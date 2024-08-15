ac7a2cbf515a14718a8805418171404300f8bc15
abel <you@example.com>
2024-08-15 18:08:42 +0000

    here we are learning how to create migrations , apply the migrations to the database. And also how to create models through python classes and ORM

--- taskly/taskly/settings.py
+++ taskly/taskly/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',	
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
 }
 
 
