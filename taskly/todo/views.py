fe9ebf53f1f51da2328d4193d68dac237dc596f0
abel <you@example.com>
2024-08-20 14:19:24 +0000

    Learned how to query database tables and how to retrive all objects and also how to retrieve specific objects

--- taskly/todo/views.py
+++ taskly/todo/views.py
@@ -1,32 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 
 # Create your views here.
 
 
 def home(request):
-    clientList = [
-       {
-          "id":1,
-          "name": "George",
-          "profession" :"web developer"
-          
-       },
-       {
-           "id":2,
-          "name": "Mike",
-          "profession" :"Architect"
-       },
-       {
-          "id":3,
-          "name": "abel",
-          "profession" :"Data scientist"
-       }
-    ]
-    context = {"client": clientList}
+    queryAlldata = Task.objects.get(id=2)  
+    context =  {'task': queryAlldata}
+    
+    return render(request, 'index.html', context = context)
 
-    return render(request, 'index.html', context=context)
+
 
 def register(request):
    return render(request, 'register.html')
