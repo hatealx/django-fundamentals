e56603f62f2e2f919574528390b887aabef56a73
abel <you@example.com>
2024-08-21 18:25:15 +0000

    For this  part  I have learnt how to include static files in my django project. css, js , Images ........

--- taskly/todo/urls.py
+++ taskly/todo/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('create_task', views.createTask, name='create_task'),
     path('view-task', views.ViewTasks, name='view-task'), 
     path('update-task/<str:pk>', views.updateTasks, name='update-task'),
-     path('delete-task/<str:pk>', views.deleteTasks, name='delete-task'),    
+    path('delete-task/<str:pk>', views.deleteTasks, name='delete-task'),    
 ]
