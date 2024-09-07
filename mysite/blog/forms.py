12bd811fc81d4e83ed08afac4840176edb9a9612
abel <you@example.com>
2024-09-07 17:15:55 +0000

    for this Section we learn how to create better url path systems (Canonial and CEO-friendly URLs)  and also learnt hoe to use models Forms and importantly implemented a COMMENT system for the blog.
    Also understood how important foreign keys are

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,8 @@
 
 
 from django import forms
+from .models import Comment
+
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
@@ -9,3 +11,10 @@ class EmailPostForm(forms.Form):
     required=False,
     widget=forms.Textarea
     )
+
+
+class CommentForm(forms.ModelForm):
+ class Meta:
+    model = Comment
+    fields = ['name', 'email', 'body']
+
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,6 +15,8 @@ class PublishedManager(models.Manager):
         super().get_queryset().filter(status=Post.Status.PUBLISHED)
         )
 
+
+
 class Post(models.Model):
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
@@ -59,6 +61,29 @@ class Post(models.Model):
             ]
         )
 
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+    Post,
+    on_delete=models.CASCADE,
+    related_name='comments'
+    )
+    
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ['created']
+        indexes = [
+        models.Index(fields=['created']),
+        ]
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
 
 
 
