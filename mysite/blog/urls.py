8e0b63f68a7e2c62bda5fab6dfc71d637dd222fc
abel <you@example.com>
2024-09-12 09:27:12 +0000

    adding a sitemap to my website  that tells search engines the pages of your website, their relevance, and how frequently they are updated

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 app_name = 'blog'
 urlpatterns = [
  # post views
@@ -11,7 +12,6 @@ urlpatterns = [
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     
-
 
 ]
 
